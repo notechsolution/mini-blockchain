2a8cf81d5bbf9c99469e551f0096981dabcdb436
notechsolution <lezhi.zeng@163.com>
2018-05-02 23:04:02 +0800

    [generator-hash]update the unittest with generator

--- generator/generator_test.go
+++ generator/generator_test.go
@@ -2,20 +2,36 @@ package generator
 
 import (
 	"testing"
-
 	"../model"
 )
 
 func TestCalculateHash(t *testing.T) {
-	block :=  model.Block{
-		BPM:       10,
-		Index:     6,
-		Timestamp: "2334455666",
-		PrevHash:  "omygodiamaprevhash",
-	} // 1062334455666omygodiamaprevhash
-
-	want := "07948e6daa751dd9295e181d58311c15f67fca504f8ad6e0600814666cc6d9b7";
-	if got := CalculateHash(block); got != want {
-		t.Errorf("CalculateHash() = %v, want %v", got, want)
+	type args struct {
+		block model.Block
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "generate hash string correctly",
+			args: args{
+				block: model.Block{
+					BPM:       10,
+					Index:     6,
+					Timestamp: "2334455666",
+					PrevHash:  "omygodiamaprevhash",
+				},
+			},
+			want: "07948e6daa751dd9295e181d58311c15f67fca504f8ad6e0600814666cc6d9b7",
+		}, // can add more test here
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CalculateHash(tt.args.block); got != tt.want {
+				t.Errorf("CalculateHash() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
