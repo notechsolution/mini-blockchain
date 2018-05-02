992b0837fc65cf9c13c015dcde6ca9dfb9b357f4
notechsolution <lezhi.zeng@163.com>
2018-05-03 07:37:10 +0800

    [generator]isValidBlock

--- generator/generator.go
+++ generator/generator.go
@@ -9,21 +9,38 @@ import (
 	"time"
 );
 
-func CalculateHash(block model.Block) string {
+type Block model.Block;
+
+func CalculateHash(block Block) string {
 	content := strconv.Itoa(block.BPM) + strconv.Itoa(block.Index) + block.Timestamp + block.PrevHash;
-	fmt.Printf("content is %s, %d, %d\n", content,block.BPM, block.Index);
+	fmt.Printf("content is %s, %d, %d\n", content, block.BPM, block.Index);
 	sha := sha256.New();
 	sha.Write([]byte(content));
 	hashed := sha.Sum(nil);
 	return hex.EncodeToString(hashed);
 }
 
-func GenerateBlock(oldBlock model.Block, BPM int) (model.Block, error) {
-	var block model.Block;
+func GenerateBlock(oldBlock Block, BPM int) (Block, error) {
+	var block Block;
 	block.BPM = BPM;
-	block.Index = oldBlock.Index +1;
+	block.Index = oldBlock.Index + 1;
 	block.PrevHash = oldBlock.Hash;
 	block.Timestamp = time.Now().String();
 	block.Hash = CalculateHash(block);
 	return block, nil;
+}
+
+func isValidBlock(newBlock, oldBlock Block) bool {
+	if newBlock.Index != oldBlock.Index+1 {
+		return false;
+	}
+
+	if newBlock.PrevHash != oldBlock.Hash {
+		return false;
+	}
+
+	if CalculateHash(newBlock) != newBlock.Hash {
+		return false;
+	}
+	return true;
 }
--- main.go
+++ main.go
@@ -1,4 +1,11 @@
 package main
 
+import (
+	"./model"
+)
+
+var Blockchain []model.Block;
+
 func main() {
+
 }
